b35a3bb5298b2b75b7ea36c541700e2a2de15043
Juergen Nickelsen <ni@w21.org>
2024-02-25 21:04:04 +0100

    have a debug function and options

--- interp/main.kt
+++ interp/main.kt
@@ -1,9 +1,38 @@
 
 import org.w21.lyk.*
 
+object Options {
+    var debug = false
+    var warn = true
+    var print_estack = false
+}
 
 
-fun main() {
+fun debug(msg: String, vararg args: Any) {
+    if (Options.debug) {
+        println("DBG $msg ${args}")
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val argl = mutableListOf<String>(*args)
+    val inOptions = true
+    
+    while (argl.size > 0 && inOptions) {
+        val arg = argl.removeFirst()
+        if (!arg.startsWith("-") || arg == "--") {
+            break
+        }
+        for (ch in arg.substring(1)) {
+            when (ch) {
+                'd' -> { Options.debug = true }
+                'e'-> { Options.print_estack }
+                else -> { println("unknown option $ch") }
+            }
+        }
+    }
+
     println(";; this is lyk")
     init_Builtins()    
     repl()
