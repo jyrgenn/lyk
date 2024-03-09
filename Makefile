5e47dd58eb549c9c9333db1b7e38a1a894c5e737
Juergen Nickelsen <ni@w21.org>
2024-03-09 22:15:12 +0100

    moved token classes out of reader.kt

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ SRCS = objects/cons.kt objects/object.kt objects/symbol.kt \
 	objects/environment.kt objects/number.kt objects/string.kt \
 	objects/table.kt objects/vector.kt objects/regexp.kt \
 	interp/exception.kt interp/eval.kt \
-	io/reader.kt io/repl.kt io/stream.kt \
+	io/reader.kt io/token.kt io/repl.kt io/stream.kt \
 	functions/function.kt functions/builtin.kt functions/lambda.kt \
 	functions/macro.kt \
 	builtins/helpers.kt builtins/numbers.kt builtins/cxr.kt \
--- io/reader.kt
+++ io/reader.kt
@@ -3,45 +3,6 @@
 package org.w21.lyk
 
 
-// Token types for the Reader. Of the most, the function is fully described by
-// their type; tokens for strings, symbols, numbers, and regexps also have some
-// value.
-
-open class ReaderToken(reader: Reader): LocationHolder {
-    val readerLocation = reader.location()
-
-    open override fun location() = readerLocation
-    open fun desc() = typeOf(this)
-    override fun toString() = desc()
-}
-
-// WARNING: don't let these inherit from another -- they must be disjoint types,
-// or otherwise the type checks depend on order, which is not good
-class OparenToken(reader: Reader): ReaderToken(reader)
-class CparenToken(reader: Reader): ReaderToken(reader) {}
-class PeriodToken(reader: Reader): ReaderToken(reader) {}
-class QuoteToken(reader: Reader): ReaderToken(reader) {}
-class FunctionToken(reader: Reader): ReaderToken(reader) {}
-class UnquoteToken(reader: Reader): ReaderToken(reader) {}
-class QuasiquoteToken(reader: Reader): ReaderToken(reader) {}
-class UnquoteSplicingToken(reader: Reader): ReaderToken(reader) {}
-class TableStartToken(reader: Reader): ReaderToken(reader) {}
-class VectorStartToken(reader: Reader): ReaderToken(reader) {}
-class StringToken(reader: Reader, val value: String): ReaderToken(reader) {
-    override fun desc() = super.desc() + "($value)"
-}
-class SymbolToken(reader: Reader, val value: String): ReaderToken(reader) {
-    override fun desc() = super.desc() + "($value)"
-}
-class NumberToken(reader: Reader, val value: Double): ReaderToken(reader) {
-    override fun desc() = super.desc() + "($value)"
-}
-class RegexpToken(reader: Reader, val value: String): ReaderToken(reader) {
-    override fun desc() = super.desc() + "($value)"
-}
-class EOFToken(reader: Reader): ReaderToken(reader) {}
-
-
 val commentChar = ';'
 
 // special characters to be escaped in string literals and similar items
