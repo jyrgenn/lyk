c7ca321719354472d076ab12df898151e4319dc1
Juergen Nickelsen <ni@w21.org>
2024-04-13 21:26:21 +0200

    pass regtests 034

--- regtests/034-env-table.lisp
+++ regtests/034-env-table.lisp
@@ -1,7 +1,7 @@
 (require 'regtests)
 
 (defvar lola 'dingeldangel)
-(test-is "env-table root" (table-get (env-table) 'lola)
+(test-is "env-table root" (table-get (env-table (root-environment)) 'lola)
          'dingeldangel)
 
 (test-is "env-table in let 1" (let ((a 'foomly))
@@ -11,10 +11,11 @@
 (defvar env1)
 (test-is "env-table in let 2" (let ((a 'foomly))
                                 (setf env1 (the-environment))
-                                (table-get (env-table) 'lola))
+                                (table-get (env-table (root-environment))
+                                           'lola))
          'dingeldangel)
 
-(test-not "from current env" ((env-table) 'a))
-(test-is "from saved env" ((env-table env1) 'a) 'foomly)
+(test-not "from current env" (table-get (env-table) 'a))
+(test-is "from saved env" (table-get (env-table env1) 'a) 'foomly)
 
 (done-testing)
