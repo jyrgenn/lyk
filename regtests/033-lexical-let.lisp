69ca69e3f07466d086f09274df8327cb431b64d7
Juergen Nickelsen <ni@w21.org>
2024-04-14 00:21:49 +0200

    033 polishing

--- regtests/033-lexical-let.lisp
+++ regtests/033-lexical-let.lisp
@@ -2,9 +2,10 @@
 
 (test-is "let nil" (let (a) a) nil)
 (test-is "setq in let" (let (a) (setq a 115) a) 115)
+
 (test-is "binding type"
          (let ((a 'lexical))                     ; binding (1)
-           (let ((f (lambda () (print a))))
+           (let ((f (lambda () (print a devnull))))
              (let ((a 'dynamic))                 ; binding (2)
                (funcall f))))
          'lexical)
