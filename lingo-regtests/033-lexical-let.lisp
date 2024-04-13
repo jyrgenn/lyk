69ee9a408a211e28998074318e2a0ee3deffc067
Juergen Nickelsen <ni@w21.org>
2024-04-13 21:21:26 +0200

    pass regtests 033

--- lingo-regtests/033-lexical-let.lisp
+++
@@ -1,6 +0,0 @@
-(require 'regtests)
-
-(test-is "let nil" (let (a) a) nil)
-(test-is "setq in let" (let (a) (setq a 115) a) 115)
-
-(done-testing)
---
+++ regtests/033-lexical-let.lisp
@@ -0,0 +1,12 @@
+(require 'regtests)
+
+(test-is "let nil" (let (a) a) nil)
+(test-is "setq in let" (let (a) (setq a 115) a) 115)
+(test-is "binding type"
+         (let ((a 'lexical))                     ; binding (1)
+           (let ((f (lambda () (print a))))
+             (let ((a 'dynamic))                 ; binding (2)
+               (funcall f))))
+         'lexical)
+
+(done-testing)
