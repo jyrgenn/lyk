8b2fab3bed96e8da0a703f9dfb1cb5b751e3aa7e
Juergen Nickelsen <ni@w21.org>
2024-05-05 11:35:18 +0200

    more tests for `(return value)`

--- regtests/067-return.lisp
+++ regtests/067-return.lisp
@@ -1,15 +1,26 @@
 ;;; return function
 
-(test-is "return fac" (progn
-                        (defun fac-ret (n)
-                          (let ((i 0)
-                                (result 1))
-                            (loop
-                             (incf i)
-                             (setf result (* i result))
-                             (when (= i n)
-                               (return result)))))
+(test-is "return fac" (flet ((fac-ret (n)
+                               "This is certainly one of the more convoluted
+ways to write a faculty function."
+                               (let ((i 0)
+                                     (result 1))
+                                 (loop
+                                  (incf i)
+                                  (setf result (* i result))
+                                  (when (= i n)
+                                    (return result))))))
                         (fac-ret 7))
          5040)
+
+;; make sure a return form in the argument list of a function does not
+;; return from *that* function, but from the outer
+(test-is "return outer" (flet ((inner (a b c)
+                                 (list c b a))
+                               (outer (b c)
+                                 (list (inner 3 4 5)
+                                       (inner b c (return "mimimi")))))
+                          (outer 'humpty 'dumpty))
+         "mimimi")
                              
 (done-testing)
