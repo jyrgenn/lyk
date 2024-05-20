876d8930a14952a8c715c828bf122ace17e8c4d4
Juergen Nickelsen <ni@w21.org>
2024-05-20 23:49:17 +0200

    pass regtests 059 (with new ~F format)

--- regtests/059-with-io.lisp
+++ regtests/059-with-io.lisp
@@ -2,16 +2,15 @@
 
 (test-is "with-output-to-string"
          (with-output-to-string (s)
-                                (print "lalala" s)
-                                ;; TODO floating point format
-                                (format s "TODO %08.3f\n" 13.4))
+           (print "lalala" s)
+           (format s "~8,3,,,'0F~%" 13.4))
          "\nlalala 0013.400\n")
 (test-is "with-input-from-string"
          (with-input-from-string (s "13.4 a #/^.*$/ 129")
-                                 (list (read s nil)
-                                       (read s nil)
-                                       (read s nil)
-                                       (read s nil)))
+           (list (read s nil)
+                 (read s nil)
+                 (read s nil)
+                 (read s nil)))
          '(13.4 a #/^.*$/ 129))
 
 (done-testing)
