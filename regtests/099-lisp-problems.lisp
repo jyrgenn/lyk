12622b701ec57a1d5f09de65326123ed10991751
Juergen Nickelsen <ni@w21.org>
2024-05-23 14:24:06 +0200

    more of the 99 Lisp problems as regtests

--- regtests/099-lisp-problems.lisp
+++ regtests/099-lisp-problems.lisp
@@ -530,4 +530,91 @@ Return the remaining elements of L1 in order."
                   (debug-vars newelem)
                   (push newelem result))))))))))
 
+;; P28 (**) Sorting a list of lists according to length of sublists
+;;
+;;     a) We suppose that a list contains elements that are lists
+;;     themselves. The objective is to sort the elements of this list
+;;     according to their length. E.g. short lists first, longer lists
+;;     later, or vice versa.
+;;
+;;     Example:
+;;     * (lsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+;;     ((O) (D E) (D E) (M N) (A B C) (F G H) (I J K L))
+;;
+;;     b) Again, we suppose that a list contains elements that are
+;;     lists themselves. But this time the objective is to sort the
+;;     elements of this list according to their length frequency;
+;;     i.e., in the default, where sorting is done ascendingly, lists
+;;     with rare lengths are placed first, others with a more frequent
+;;     length come later.
+;;
+;;     Example:
+;;     * (lfsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+;;     ((I J K L) (O) (A B C) (F G H) (D E) (D E) (M N))
+;;
+;;     Note that in the above example, the first two lists in the
+;;     result have length 4 and 1, both lengths appear just once. The
+;;     third and forth list have length 3 which appears twice (there
+;;     are two list of this length). And finally, the last three lists
+;;     have length 2. This is the most frequent length.
+
+
+;; To make this really match the given solution, there is a hidden
+;; requirement that the parts that compare equal be alphabetically
+;; sorted. That makes it a bit more complex than (I guess) intended.
+
+(defun lsort (l)
+  (flet ((lcomp (a b)           
+           (let ((la (length a))
+                 (lb (length b)))
+             (if (= la lb)
+                 (< (string a) (string b))
+                 (< la lb)))))
+    (sort l #'lcomp)))
+
+(test-is "lisp problem 28/99 b"
+         (lsort '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+         '((o) (d e) (d e) (m n) (a b c) (f g h) (i j k l)))
+
+(defun sort-frequency (lists)
+  (let ((len-list '()))
+    (dolist (l lists)
+      (let* ((len (length l))
+             (pair (assoc len len-list)))
+        (if (null pair)
+            (push (cons len (list l)) len-list)
+            (push l (cdr pair)))))
+    (flet ((lcomp (a b)           
+             (let ((la (length a))
+                   (lb (length b)))
+               (if (= la lb)
+                   (< (string a) (string b))
+                   (< la lb)))))
+      (apply #'append (map (lambda (l)
+                             (sort l
+                                   (lambda (a b) (< (string a) (string b)))))
+                           (sort (map #'cdr len-list) #'lcomp))))))
+     
+(test-is "lisp problem 28/99 a"
+         (sort-frequency '((a b c) (d e) (f g h) (d e) (i j k l) (m n) (o)))
+         '((i j k l) (o) (a b c) (f g h) (d e) (d e) (m n)))
+
+
+(defun is-prime (n)
+  (cond ((< n 2) nil)
+        ((= n 2) t)
+        ((zerop (% n 2)) nil)
+        (t (let ((divisor 3)
+                 (limit (isqrt n)))
+             (while (<= divisor limit)
+               (if (zerop (% n divisor))
+                   (return nil))
+               (incf divisor 2))
+             t))))
+             
+(test "lisp problem 31/99" (is-prime 7))
+  
+  
+
+
 (done-testing)
