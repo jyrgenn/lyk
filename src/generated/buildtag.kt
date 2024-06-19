b6dcbfa5bd32cb7d9855be45cc1231e43d0b02d0
Juergen Nickelsen <ni@w21.org>
2024-06-19 22:58:43 +0200

    moved all sources to src/; wrote INSTALL instructions

---
+++ src/generated/buildtag.kt
@@ -0,0 +1,18 @@
+// Build tag -- generated file, do not edit
+
+package org.w21.lyk
+
+val build_info = mapOf(
+    "program" to "lyk",
+    "version" to "v0.2-139-g9c4784e-dirty",
+    "kotlin" to "kotlinc-jvm 2.0.0",
+    "built-at" to "2024-06-19 22:57",
+    "built-by" to "ni@abild",
+)
+
+val installdir = "/opt/w21/lib/lyk"
+
+
+fun buildtag(): String {
+    return "lyk v0.2-139-g9c4784e-dirty kotlinc-jvm 2.0.0 2024-06-19 22:57 ni@abild"
+}
